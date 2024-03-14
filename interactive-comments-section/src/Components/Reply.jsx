6cd9e204a7b6613fe98d0fd98b1e1980ff933648
borgesmj <borgesmj19@gmail.com>
2024-03-14 07:53:07 -0500

    adding voting function to the replies

--- interactive-comments-section/src/Components/Reply.jsx
+++ interactive-comments-section/src/Components/Reply.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Reply = ({userName, currentUser, content, score, createdAt, profilePic}) => {
+const Reply = ({
+  userName,
+  currentUser,
+  content,
+  score,
+  createdAt,
+  profilePic,
+}) => {
+  const [replyScore, setReplyScore] = useState(score);
 
   const activeUser = () => {
-    if (userName === currentUser)
-    return true
-  }
+    if (userName === currentUser) return true;
+  };
+
+  const handleVote = () => {
+    setReplyScore(replyScore + 1);
+  };
+
+  const handleUnVote = () => {
+    if (replyScore > 0) {
+      setReplyScore(replyScore - 1);
+    } else {
+      return;
+    }
+  };
   return (
     <div className="bg-color-white p-4 rounded-[10px] flex flex-col my-4 relative">
-      <div className={`${!activeUser() ? 'w-4/5 lg:w-[30%]' : 'w-full lg:w-2/5'} flex flex-row justify-between items-center mb-2 lg:ml-[50px]`}>
+      <div
+        className={`${
+          !activeUser() ? "w-full lg:w-[30%]" : "w-full lg:w-2/5"
+        } flex flex-row justify-between items-center mb-2 lg:ml-[50px]`}
+      >
         <img
           src={profilePic}
           alt="profile_user_image"
@@ -17,25 +40,34 @@ const Reply = ({userName, currentUser, content, score, createdAt, profilePic}) =
         <a href="#" className="font-bold">
           {userName}
         </a>
-        <span className={`bg-Moderate-blue py-[2px] px-[6px] text-[14px] font-bold text-white rounded ${!activeUser() ? "hidden" : null}`}>
+        <span
+          className={`bg-Moderate-blue py-[2px] px-[6px] text-[14px] font-bold text-white rounded ${
+            !activeUser() ? "hidden" : null
+          }`}
+        >
           you
         </span>
         <span className="txt-Grayish-Blue">{createdAt}</span>
       </div>
-      <p className="my-2 txt-Grayish-Blue lg:ml-[50px]">
-        {content}
-      </p>
+      <p className="my-2 txt-Grayish-Blue lg:ml-[50px]">{content}</p>
       <div className="bg-Very-light-gray p-[5px] w-[30%] txt-Moderate-blue font-bold flex flex-row justify-between rounded-[8px] text-[18px] lg:flex-col lg:w-[40px] lg:absolute lg:justify-center">
-        <button className="w-1/4 lg:text-center lg:w-full">+</button>
+        <button className="w-1/4 lg:text-center lg:w-full" onClick={handleVote}>
+          +
+        </button>
         <input
           type="text"
           name=""
           id=""
-          value={score}
+          value={replyScore}
           className="w-1/4 bg-transparent lg:text-center lg:w-full outline-none"
           readOnly
         />
-        <button className="w-1/4 lg:text-center lg:w-full">-</button>
+        <button
+          className="w-1/4 lg:text-center lg:w-full"
+          onClick={handleUnVote}
+        >
+          -
+        </button>
       </div>
       <div className=" absolute bottom-[20px] right-[20px] lg:top-[25px]">
         {!activeUser() ? (
@@ -57,7 +89,7 @@ const Reply = ({userName, currentUser, content, score, createdAt, profilePic}) =
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Reply
+export default Reply;
