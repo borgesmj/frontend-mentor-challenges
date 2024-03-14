8a920356f18d7abed186aeaf827054ad0d1b354c
borgesmj <borgesmj19@gmail.com>
2024-03-14 15:36:40 -0500

    saving in localstorage

--- interactive-comments-section/src/App.jsx
+++ interactive-comments-section/src/App.jsx
@@ -2,100 +2,53 @@ import { useEffect, useState } from "react";
 import Comments from "./Components/Comments";
 import Form from "./Components/Form";
 
-function App() {
+const  App = () =>  {
   const [currentUser, setCurrentUser] = useState({});
   const [usersComments, setUserComments] = useState([]);
   const [newComment, setNewComment] = useState({});
 
   useEffect(() => {
-    const obtenerLS = JSON.parse(localStorage.getItem("commentsData"));
+    const getComments = () => JSON.parse(localStorage.getItem("comments"));
+    const getActiveUser = () => JSON.parse(localStorage.getItem("activeUser"));
 
-    if (obtenerLS) {
-      setUserComments(obtenerLS.comments);
-      setCurrentUser(obtenerLS.currentUser);
+    const commentsLS = getComments();
+    const activeUserLS = getActiveUser();
+
+    if (commentsLS?.length > 0 && Object.keys(activeUserLS).length > 0) {
+      setUserComments(commentsLS);
+      setCurrentUser(activeUserLS);
     } else {
-      const initialData = {
-        currentUser: {
-          image: {
-            png: "./images/avatars/image-juliusomo.png",
-            webp: "./images/avatars/image-juliusomo.webp",
-          },
-          username: "juliusomo",
-        },
-        comments: [
-          {
-            id: 1,
-            content:
-              "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-            createdAt: "1 month ago",
-            score: 12,
-            user: {
-              image: {
-                png: "./images/avatars/image-amyrobson.png",
-                webp: "./images/avatars/image-amyrobson.webp",
-              },
-              username: "amyrobson",
-            },
-            replies: [],
-          },
-          {
-            id: 2,
-            content:
-              "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-            createdAt: "2 weeks ago",
-            score: 5,
-            user: {
-              image: {
-                png: "./images/avatars/image-maxblagun.png",
-                webp: "./images/avatars/image-maxblagun.webp",
-              },
-              username: "maxblagun",
-            },
-            replies: [
-              {
-                id: 3,
-                content:
-                  "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-                createdAt: "1 week ago",
-                score: 4,
-                replyingTo: "maxblagun",
-                user: {
-                  image: {
-                    png: "./images/avatars/image-ramsesmiron.png",
-                    webp: "./images/avatars/image-ramsesmiron.webp",
-                  },
-                  username: "ramsesmiron",
-                },
-              },
-              {
-                id: 4,
-                content:
-                  "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-                createdAt: "2 days ago",
-                score: 2,
-                replyingTo: "ramsesmiron",
-                user: {
-                  image: {
-                    png: "./images/avatars/image-juliusomo.png",
-                    webp: "./images/avatars/image-juliusomo.webp",
-                  },
-                  username: "juliusomo",
-                },
-              },
-            ],
-          },
-        ],
-      };
-
-      localStorage.setItem("commentsData", JSON.stringify(initialData));
-      setUserComments(initialData?.comments);
-      setCurrentUser(initialData?.currentUser);
+      fetchCommentsData();
     }
   }, []);
 
   const createComment = (comment) => {
-    setUserComments(prevComments => [...prevComments, comment]);
+    setUserComments((prevComments) => [...prevComments, comment]);
+    localStorage.setItem('comments', JSON.stringify(usersComments))
   };
+
+  const fetchCommentsData = async () => {
+    try {
+      const response = await fetch("./data.json");
+      if (!response.ok) {
+        throw new Error("Failed to fetch comments data");
+      }
+      const data = await response.json();
+      setCurrentUser(data.currentUser);
+      setUserComments(data.comments);
+    } catch (error) {
+      console.error("Error al cargar los datos:", error);
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("comments", JSON.stringify(usersComments));
+  }, [usersComments]);
+
+  useEffect(() => {
+    localStorage.setItem("activeUser", JSON.stringify(currentUser));
+  }, [currentUser]);
+
   return (
     <main className="w-full py-8 px-4 md:w-3/4 md:my-0 md:mx-auto lg:w-1/2">
       <Comments
@@ -106,8 +59,8 @@ function App() {
         profilePic={currentUser?.image?.webp}
         createComment={createComment}
         setNewComment={setNewComment}
-        currentUser = {currentUser}
-        newComment = {newComment}
+        currentUser={currentUser}
+        newComment={newComment}
       />
     </main>
   );
